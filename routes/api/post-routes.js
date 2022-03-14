f0a0cd6c3cc48b279cb26a74b2a2efe89bf3fea5
Raul <jimenezraul1981@gmail.com>
2022-03-14 15:31:46 -0400

    enable users to vote on posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,17 +1,22 @@
 const router = require("express").Router();
-const { includes } = require("lodash");
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const sequelize = require("../../config/connection");
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
-    order: [["created_at", "DESC"]],
-    include: [
-      {
-        model: User,
-        attributes: ["username"],
-      },
+    // update the `.findAll()` method's attributes to look like this
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
     ],
   })
     .then((dbPostData) => res.json(dbPostData))
@@ -26,7 +31,18 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -61,23 +77,34 @@ router.post("/", (req, res) => {
     });
 });
 
+// PUT /api/posts/upvote
+router.put("/upvote", (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 router.put("/:id", (req, res) => {
   Post.update(
     {
       title: req.body.title,
+    },
+    {
+      where: {
+        id: req.params.id,
       },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
+    }
   )
     .then((dbPostData) => {
       if (!dbPostData) {
         res.status(404).json({ message: "No post found with this id" });
         return;
       }
-      res.json(dbPostData)
+      res.json(dbPostData);
     })
     .catch((err) => {
       console.log(err);
